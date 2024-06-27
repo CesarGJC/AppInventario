55c8010349205e47abe5a3d244facc7dd4b99cce
sebascc07 <154444925+sebascc07@users.noreply.github.com>
2024-06-27 11:57:22 -0400

    Agregar y editar cli

--- CmpInventario/Cliente.vb
+++ CmpInventario/Cliente.vb
@@ -3,5 +3,14 @@
         Dim adap As New BDTableAdapters.ClientesTableAdapter
         adap.BuscarCliente(ds, cli)
     End Sub
+    Public Sub AgregarCliente(ci As String, nombre As String, ape As String, tel As String, ds As BD.ClientesDataTable)
+        Dim adap As New BDTableAdapters.ClientesTableAdapter
+        adap.AgregarCliente(ci, nombre, ape, tel)
+    End Sub
 
+    Public Sub EditarCliente(ci As String, nombre As String, ape As String, tel As String, ds As BD.ClientesDataTable)
+        Dim adap As New BDTableAdapters.ClientesTableAdapter
+        adap.EditarCliente(ci, nombre, ape, tel)
+
+    End Sub
 End Class
